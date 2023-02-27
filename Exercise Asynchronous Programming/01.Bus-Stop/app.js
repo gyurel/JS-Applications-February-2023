e1b81c994f0a98f52ce82f544a396eecc51199b4
gyurel <gyurel@yahoo.com>
2023-02-27 12:32:28 +0100

    problem 1

--- Exercise Asynchronous Programming/01.Bus-Stop/app.js
+++ Exercise Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,30 @@
 function getInfo() {
-    console.log("TODO...");
+    // location.reload();
+    let sotIdInputField = document.getElementById("stopId");
+    let stopId = sotIdInputField.value;
+    if(stopId == ""){
+        return;
+    }
+
+    let bussesUl = document.getElementById("buses");
+    bussesUl.innerHTML = "";
+    let stopNameDiv = document.getElementById("stopName");
+    stopNameDiv.textContent = "";
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+    .then((response) => response.json())
+    .then((data) =>{
+        // console.log(data);
+        sotIdInputField.value = "";
+        stopNameDiv.textContent = data["name"];
+        for (const [key, val] of Object.entries(data.buses)) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${key} arrives in ${val} minutes`
+            bussesUl.appendChild(li);
+        }
+    })
+    .catch((error) => {
+        sotIdInputField.value = "";
+        stopNameDiv.textContent = `${"Error"}`;
+    })
 }
