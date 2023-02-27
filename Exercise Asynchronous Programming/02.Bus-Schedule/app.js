3a1ba662102af29bb13f17bb5a505ddd00133e5c
gyurel <gyurel@yahoo.com>
2023-02-27 21:49:11 +0100

    problem 2

--- Exercise Asynchronous Programming/02.Bus-Schedule/app.js
+++ Exercise Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,38 @@
 function solve() {
 
+    let currentStop = "depot";
+    let nextStop;
+    let infoDiv = document.querySelector("#info span.info");
+    let departButton = document.getElementById("depart");
+    let arriveButton = document.getElementById("arrive");
+
+    let currnetData;
+
+
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`)
+        .then((response) => response.json())
+        .then((data) =>{
+            currnetData = data;
+            // console.log(data);
+            infoDiv.textContent = `Next stop ${data.name}`;
+            nextStop = data.next;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+        })
+        .catch((error) => {
+            departButton.disabled = true;
+            arriveButton.disabled = true;
+            infoDiv.textContent = "Error";
+        })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoDiv.textContent = `Arriving at ${currnetData.name}`;
+        nextStop = currnetData.next;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+        currentStop = nextStop
     }
 
     return {
